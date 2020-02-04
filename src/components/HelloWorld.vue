58a7898f6702a71268a408f08ec614d140cbf800
AndyWu <wumin@sybercare.com>
2020-02-04 19:23:00 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -51,7 +51,7 @@ export default {
   methods: {
    startHacking () {
       this.$notify({
-        title: 'It works 1212!',
+        title: 'It works 1211112!',
         type: 'success',
         message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
         duration: 5000
